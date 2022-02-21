84f31269eb22ff302ddf425b07b7b0be0030f858
tom montbord <montbordino@gmail.com>
2022-02-21 19:09:27 +0100

    ajout de quelques variables et de 'choisirPays'

--- script.js
+++ script.js
@@ -2,28 +2,52 @@ const FR = ['do', 're', 'mi', 'fa', 'sol', 'la', 'si'];
 const EN = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
 const NB_QUESTION = 10;
 let ecrit = "";
+let i = 1;
+let pays;
+let noteCode;
 
-function quizz()
+function choisirPays()
 {
-    for (let i = 1; i <= NB_QUESTION; i++){
-        document.getElementById("question" + i).innerHTML = i;
+    let paysCode = Math.floor(Math.random() * 2);
+    if(paysCode === 0){
+        pays = "FR";
     }
+    else{
+        pays = "EN";
+    }
+    console.log(pays);
 }
 
-/* pour saisir du texte
+
+function quizz()
+{    
+    window.addEventListener('keydown', (event) => {
+        ecrit += event.key;
+        if (event.key == "Enter")
+        {
+            i++;
+            ecrit = "";
+            console.log(i);
+        }
+        else
+        {
+            document.getElementById("q" + i).innerHTML = ecrit;
+        }
+    })
+}
+
+/*
+
+// pour ecrire du texte
+document.getElementById("question" + i).innerHTML = ecrit + " | ";
+
+// pour saisir du texte
 window.addEventListener('keydown', (event) => {
 
     if (event.key == "r")
     {
         color = "red";
     }
-    if (event.key == "ArrowLeft")
-    {
-        left -= vitesse;
-    }
-    if (event.key == "ArrowRight")
-    {
-        left += vitesse;
-    }
+
 })
 */
