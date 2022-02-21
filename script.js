6d2f0d0f58212e055c8f7207e52e9194e8263c43
tom montbord <montbordino@gmail.com>
2022-02-21 23:12:37 +0100

    verification de la victoire sur une question et transformation du bg en vert

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+// bug avec entrer/espace
+
 const FR = ['do', 're', 'mi', 'fa', 'sol', 'la', 'si'];
 const EN = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
 const NB_QUESTION = 10;
 let reponse = "";
+let reponseAttendue;
 let pays;
 let noteCode;
 let i = 1;
@@ -17,21 +20,36 @@ function choisirPays()
     }
 }
 
+function defRep()
+{
+    if(pays == FR){
+        reponseAttendue = EN[noteCode];
+    }
+    else{
+        reponseAttendue = FR[noteCode];
+    }
+}
+
+function verifV()
+{
+    if(reponse == reponseAttendue){
+        document.getElementById("q" + i).className = "valide";
+    } 
+}
+
 function quizz()
 {
     choisirPays(); //choisi un pays au hasard dans "pays"
     noteCode = Math.floor(Math.random() * 7); // noteCode est mtn un nombre entre 0 et 6
+    defRep();
+    
     let question = "l'équivalent de " + pays[noteCode] + " : "; // chois du message
     document.getElementById("q" + i).innerHTML = question; // affiche la question
 
     window.addEventListener('keydown', (event) => {
-
         if(event.key == "Backspace"){
             reponse = reponse.slice(0, -1);
             document.getElementById("q" + i).innerHTML = question + reponse;
-        }
-        else if(event.key == Enter){
-            console.log("ca marche");
         }
         else
         {
@@ -39,9 +57,9 @@ function quizz()
             document.getElementById("q" + i).innerHTML += event.key; // affiche la réponse
         }
         console.log(reponse);
+
+        verifV();
     })
-
-
 }
 
 /*
