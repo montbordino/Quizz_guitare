34e9f7368361487c08ad2a8432349016255ca02b
tom montbord <montbordino@gmail.com>
2022-02-21 22:45:42 +0100

    ajout du système question/réponse (code non fonctionnel)

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
 const FR = ['do', 're', 'mi', 'fa', 'sol', 'la', 'si'];
 const EN = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
 const NB_QUESTION = 10;
-let ecrit = "";
-let i = 1;
+let reponse = "";
 let pays;
 let noteCode;
+let i = 1;
 
 function choisirPays()
 {
     let paysCode = Math.floor(Math.random() * 2);
     if(paysCode === 0){
-        pays = "FR";
+        pays = FR;
     }
     else{
-        pays = "EN";
+        pays = EN;
     }
-    console.log(pays);
 }
 
-
 function quizz()
-{    
+{
+    choisirPays(); //choisi un pays au hasard dans "pays"
+    noteCode = Math.floor(Math.random() * 7); // noteCode est mtn un nombre entre 0 et 6
+    let question = "l'équivalent de " + pays[noteCode] + " : "; // chois du message
+    document.getElementById("q" + i).innerHTML = question; // affiche la question
+
     window.addEventListener('keydown', (event) => {
-        ecrit += event.key;
-        if (event.key == "Enter")
-        {
-            i++;
-            ecrit = "";
-            console.log(i);
+
+        if(event.key == "Backspace"){
+            reponse = reponse.slice(0, -1);
+            document.getElementById("q" + i).innerHTML = question + reponse;
+        }
+        else if(event.key == Enter){
+            console.log("ca marche");
         }
         else
         {
-            document.getElementById("q" + i).innerHTML = ecrit;
+            reponse += event.key;
+            document.getElementById("q" + i).innerHTML += event.key; // affiche la réponse
         }
+        console.log(reponse);
     })
+
+
 }
 
 /*
